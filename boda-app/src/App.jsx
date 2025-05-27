6ec6f18de4ae35528bf11ff60480a08320f06829
shonko joseph <josephshonko99@gmail.com>
2025-05-27 13:12:42 +0300

    on app.jsx ive add all the imports

--- boda-app/src/App.jsx
+++ boda-app/src/App.jsx
@@ -1,5 +1,22 @@
-function App(){
-  return <div className="App">Hello There</div>
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Community from './pages/Community';
+import Business from './pages/Business';
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/business" element={<Business />} />
+      </Routes>
+    </Router>
+  );
+}
